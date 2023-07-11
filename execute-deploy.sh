9fd96dc070535411ed47984dcda2cd1b0067d574
hyeonju-kim <yocu1784@naver.com>
2023-07-11 20:30:35 +0900

    [230711] 배포 hooks 추가

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,4 +1,4 @@
 #CodeDeploy에서 바로 deploy.sh를 실행시킬수 없어 우회하는 방법으로 deploy.sh를 실행하는 execute-deploy.sh파일을 실행하도록 설정
 
 #!/bin/bash
-/home/ec2-user/app/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+/home/ubuntu/app/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
