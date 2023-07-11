c17760836c74f329db66fce13acfc46f98bc4f30
hyeonju-kim <yocu1784@naver.com>
2023-07-11 21:19:32 +0900

    [230711] 배포 hooks 추가

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
-
-REPOSITORY=/home/ubuntu/app/travis
-
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -f winterfoodies_project)
-
-echo "$CURRENT_PID"
-
-if [ -z $CURRENT_PID ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
-fi
-
-echo "> 새 어플리케이션 배포"
-
-echo "> Build 파일 복사"
-
-cp $REPOSITORY/build/target/*.jar $REPOSITORY/jar/
-
-JAR_NAME=$(ls $REPOSITORY/jar/ |grep 'winterfoodies_project' | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME"
-
-nohup java -jar $REPOSITORY/jar/$JAR_NAME &
+##!/bin/bash
+#
+#REPOSITORY=/home/ubuntu/app/travis
+#
+#echo "> 현재 구동중인 애플리케이션 pid 확인"
+#
+#CURRENT_PID=$(pgrep -f winterfoodies_project)
+#
+#echo "$CURRENT_PID"
+#
+#if [ -z $CURRENT_PID ]; then
+#    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+#else
+#    echo "> kill -15 $CURRENT_PID"
+#    kill -15 $CURRENT_PID
+#    sleep 5
+#fi
+#
+#echo "> 새 어플리케이션 배포"
+#
+#echo "> Build 파일 복사"
+#
+#cp $REPOSITORY/build/target/*.jar $REPOSITORY/jar/
+#
+#JAR_NAME=$(ls $REPOSITORY/jar/ |grep 'winterfoodies_project' | tail -n 1)
+#
+#echo "> JAR Name: $JAR_NAME"
+#
+#nohup java -jar $REPOSITORY/jar/$JAR_NAME &
