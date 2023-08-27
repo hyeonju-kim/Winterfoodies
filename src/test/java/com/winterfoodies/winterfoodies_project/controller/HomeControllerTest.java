8362c5738aaec8a6493671565707d7e1adbda3b1
hyeonju-kim <yocu1784@naver.com>
2023-08-27 18:23:05 +0900

    [230827] REDIS 운영서버 적용

---
+++ src/test/java/com/winterfoodies/winterfoodies_project/controller/HomeControllerTest.java
@@ -0,0 +1,20 @@
+package com.winterfoodies.winterfoodies_project.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomeControllerTest {
+
+    @Test
+    public void loginTest() {
+    }
+
+    @Test
+    public void signUp() {
+    }
+
+    @Test
+    public void logout() {
+    }
+}
