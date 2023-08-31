640c921c11dc25049464a12adb1c59a90c7762cd
hyeonju-kim <yocu1784@naver.com>
2023-08-31 11:04:44 +0900

    [230831] 스웨거에 헤더설정추가

--- src/main/java/com/winterfoodies/winterfoodies_project/config/SwaggerConfig.java
+++ src/main/java/com/winterfoodies/winterfoodies_project/config/SwaggerConfig.java
@@ -1,21 +1,36 @@
 package com.winterfoodies.winterfoodies_project.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
+import java.util.List;
+
+@CrossOrigin
 @Configuration
-public class SwaggerConfig {
+@RequiredArgsConstructor
+public class SwaggerConfig { // 스웨거 버전 3.0.0
     @Bean
     public Docket api() {
 
 
         return new Docket(DocumentationType.OAS_30)
+                // 헤더 설정을 위해 3줄 추가 - 230831
+                .useDefaultResponseMessages(false) //Swagger에서 제공해주는 기본 응답 코드 받지 않음 설정
+                .securityContexts(List.of(this.securityContext())) // SecurityContext 설정
+                .securitySchemes(List.of(this.apiKey())) // ApiKey 설정
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
@@ -31,4 +46,27 @@ public class SwaggerConfig {
                 .version("0.1")
                 .build();
     }
+
+    // 헤더 설정을 위해 추가 - 230831
+
+    // JWT SecurityContext 구성
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    //Authorization을 인증 헤더로 포함하도록 ApiKey를 정의
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+
 }
