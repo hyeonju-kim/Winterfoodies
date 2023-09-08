a748a41fc9fda70644ddc4e7cf580792329e6a46
hyeonju-kim <yocu1784@naver.com>
2023-09-08 23:42:42 +0900

    [230908] AuthService 리팩토링

--- src/main/java/com/winterfoodies/winterfoodies_project/service/AuthService.java
+++ src/main/java/com/winterfoodies/winterfoodies_project/service/AuthService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.security.SecureRandom;
-import java.util.Random;
+
 
 @Service
 @RequiredArgsConstructor
